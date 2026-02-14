38e7f0113d13c10de2065bc4ed84474cb82e8e11
Juliusouko <julius.ouko@student.moringaschool.com>
2026-02-14 12:38:55 +0300

    nice landing page with newsletter for email subscription

---
+++ src/js/subscribe.js
@@ -0,0 +1,42 @@
+const form = document.querySelector("form");
+const toast = document.getElementById("toast");
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  const email = form.email.value;
+  const button = form.querySelector("button");
+  button.disabled = true;
+
+  try {
+    const response = await fetch(
+      "https://api.aswito.com/subscribe",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email }),
+      },
+    );
+
+    const data = await response.json();
+
+    toast.innerText = data.message;
+    toast.style.backgroundColor = "#4CAF50";
+    toast.classList.add("show");
+
+    form.reset();
+
+    setTimeout(() => toast.classList.remove("show"), 3000);
+  } catch (error) {
+    console.error(error);
+    toast.innerText = "Something went wrong. Please try again.";
+    toast.style.backgroundColor = "f44336";
+    toast.classList.add("show");
+
+    setTimeout(() => {
+      toast.classList.remove("show");
+    }, 3000);
+  } finally {
+    button.disabled = false;
+  }
+});
